01da2dfc9c99b3550ca6824a49751a134886ba0f
Abhijay Das <abhijayd@umich.edu>
2025-11-09 13:14:47 -0500

    Added % change column and logic to check the % change at each interval

--- stockTracker.py
+++ stockTracker.py
@@ -1,7 +1,17 @@
 import yfinance as yf
+# import pandas as pd
+# import time
 
-ticker = yf.Ticker("SPY")
-spy_price = ticker.history(period='1d', interval='30m')
+ticker = "EVMN"
+data = yf.download(ticker, start="2025-11-07", end="2025-11-08", interval="30m").tz_convert('America/New_York')
+data['Time-EST'] = data.index.strftime("%I:%M %p")
+data['% Change'] = (data['Close'] / data['Open']) * 100 - 100
+data = data.round(2)
 
-print(spy_price)
+for index, row in data.iterrows():
+    if (row['% Change'].item() > 1):
+        print("Good time to buy! Change is ", row['% Change'].item(), "%")
+    else:
+        print("Don't Buy! Change is ", row['% Change'].item(), "%")
 
+print(data[['Time-EST', 'Open', 'Close', '% Change']])
