deca071720634c3bcec13448f568d03a834b8d26
Abhijay Das <abhijayd@umich.edu>
2025-11-09 14:26:31 -0500

    Modified to only return the most recent %change

--- stockTracker.py
+++ stockTracker.py
@@ -1,16 +1,10 @@
 import yfinance as yf
 import time
-
 def get_percent_changes(data):
 
     data['% Change'] = (data['Close'] / data['Open']) * 100 - 100
     data = data.round(2)
-
-    for _, row in data.iterrows():
-        if (row['% Change'].item() > 1):
-            print(row['Time-EST'].item(), ": Good time to buy! Change is ", row['% Change'].item(), "%")
-        else:
-            print(row['Time-EST'].item(), ":Don't Buy! Change is ", row['% Change'].item(), "%")
+    data = data.iloc[-1]
 
     return data
 
@@ -18,8 +12,6 @@ ticker = "EVMN"
 data = yf.download(ticker, period="1d", interval="30m").tz_convert('America/New_York')
 data['Time-EST'] = data.index.strftime("%I:%M %p")
 processed = get_percent_changes(data)
+print(processed)
 
-
-data = data.round(2)
-
-print(data[['Time-EST', 'Open', 'Close', '% Change']])
+# print(data[['Time-EST', 'Open', 'Close', '% Change']])
