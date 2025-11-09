00dd19891accd50640b39cf74b1bc89862f98909
Abhijay Das <abhijayd@umich.edu>
2025-11-09 13:56:40 -0500

    Added a function to calculate and return percent change

--- stockTracker.py
+++ stockTracker.py
@@ -1,17 +1,25 @@
 import yfinance as yf
-# import pandas as pd
-# import time
+import time
+
+def get_percent_changes(data):
+
+    data['% Change'] = (data['Close'] / data['Open']) * 100 - 100
+    data = data.round(2)
+
+    for _, row in data.iterrows():
+        if (row['% Change'].item() > 1):
+            print(row['Time-EST'].item(), ": Good time to buy! Change is ", row['% Change'].item(), "%")
+        else:
+            print(row['Time-EST'].item(), ":Don't Buy! Change is ", row['% Change'].item(), "%")
+
+    return data
 
 ticker = "EVMN"
-data = yf.download(ticker, start="2025-11-07", end="2025-11-08", interval="30m").tz_convert('America/New_York')
+data = yf.download(ticker, period="1d", interval="30m").tz_convert('America/New_York')
 data['Time-EST'] = data.index.strftime("%I:%M %p")
-data['% Change'] = (data['Close'] / data['Open']) * 100 - 100
-data = data.round(2)
+processed = get_percent_changes(data)
 
-for index, row in data.iterrows():
-    if (row['% Change'].item() > 1):
-        print("Good time to buy! Change is ", row['% Change'].item(), "%")
-    else:
-        print("Don't Buy! Change is ", row['% Change'].item(), "%")
+
+data = data.round(2)
 
 print(data[['Time-EST', 'Open', 'Close', '% Change']])
