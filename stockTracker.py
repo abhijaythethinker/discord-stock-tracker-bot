0e7f020efbc6c54f8578a7f6f033201c61d3f362
Abhijay Das <abhijayd@umich.edu>
2025-11-09 22:59:06 -0500

    Combined everything into one function for the bot

--- stockTracker.py
+++ stockTracker.py
@@ -1,17 +1,20 @@
 import yfinance as yf
-import time
-def get_percent_changes(data):
+def get_percent_changes():
+
+    ticker = "EVMN"
+    data = yf.download(ticker, period="1d", interval="30m", auto_adjust=False, progress=False).tz_convert('America/New_York')
+
+    if data.empty:
+        return None
+    
+    data['Time-EST'] = data.index.strftime("%I:%M %p")
 
     data['% Change'] = (data['Close'] / data['Open']) * 100 - 100
     data = data.round(2)
-    data = data.iloc[-1]
 
-    return data
+    latest = data.iloc[-1]
 
-ticker = "EVMN"
-data = yf.download(ticker, period="1d", interval="30m").tz_convert('America/New_York')
-data['Time-EST'] = data.index.strftime("%I:%M %p")
-processed = get_percent_changes(data)
-print(processed)
+    return latest
 
-# print(data[['Time-EST', 'Open', 'Close', '% Change']])
+result = get_percent_changes()
+print(result)
