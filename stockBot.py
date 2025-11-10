c5c4a41873ee8b6b79f7bd9043783a83f83218f9
Abhijay Das <abhijayd7@gmail.com>
2025-11-10 17:58:13 -0500

    Bot only sends messages every 30 minutees 9-5 mon-fri

--- stockBot.py
+++ stockBot.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands, tasks
 import os
 from dotenv import load_dotenv
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.cron import CronTrigger
 from stockTracker import get_percent_changes
 
 load_dotenv()
@@ -12,18 +14,30 @@ intents = discord.Intents.default()
 
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.default())
 
+scheduler = AsyncIOScheduler()
+
 @bot.event
 async def on_ready():
     print(f"Bot is online as {bot.user}")
-    send_message.start()
+    
+    if not scheduler.running:
+        scheduler.start()
 
-@tasks.loop(minutes=30)
 async def send_message():
-    await bot.wait_until_ready()
     channel = bot.get_channel(CHANNEL_ID)
     if channel:
         await channel.send(get_percent_changes("SPY"))
+        await channel.send(get_percent_changes("QQQ"))
     else:
         print(f"Channel not found.")
+
+scheduler.add_job(
+    send_message,
+    CronTrigger(
+        day_of_week="mon-fri",
+        hour="9-15",
+        minute="35"
+    )
+)
 
 bot.run(TOKEN)
--- stockTracker.py
+++ stockTracker.py
@@ -10,13 +10,15 @@ def get_percent_changes(ticker):
     
     data['Time-EST'] = data.index.strftime("%I:%M %p")
 
-    # calculate percent change
-    data['% Change'] = (data['Close'] / data['Open']) * 100 - 100
+    open_price = data.iloc[0]
+
+    # calculate percent
+    data['% Change'] = (data['Close'] / open_price['Open']) * 100 - 100
     data = data.round(2)
 
     latest = data.iloc[-1]
 
-    open_price = float(latest['Open'].iloc[0])
+    open_price = float(open_price['Open'].iloc[0])
     close_price = float(latest['Close'].iloc[0])
     percent_change = float(latest['% Change'].iloc[0])
     time_str = str(latest['Time-EST'].iloc[0])
@@ -24,9 +26,9 @@ def get_percent_changes(ticker):
     message = (
         f"**{ticker} Stock Update**\n"
         f"Time: {time_str}\n"
-        f"% Change: {percent_change}%\n"
-        f"Open: ${open_price}\n"
-        f"Close: ${close_price}\n"
+        f"% Change: {percent_change:.2f}%\n"
+        f"Open: ${open_price:.2f}\n"
+        f"Close: ${close_price:.2f}\n"
     )
     return message
 
